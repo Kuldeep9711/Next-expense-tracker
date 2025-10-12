8ba409ed7e60e1ed0f35813ac52690b4ebefa99d
Kuldeep Saini <k72125642@gmail.com>
2025-10-13 00:24:35 +0530

    add navbar components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
-export default function HomePage() {
+import { currentUser } from "@clerk/nextjs/server";
+import Guest from "./components/Guest";
+
+export default async function HomePage() {
+  const user = await currentUser();
+
+  if(!user) {
+    return <Guest />;
+  }
+
   return(
-    <div className='text-red-400'>HomePage</div>
-  )
+    <div className='text-red-400 dark:text-red-300'>
+      <h1>HomePage</h1>
+      </div>
+  );
 }
