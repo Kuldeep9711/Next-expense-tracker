2562dd9c447213af3dd0b26172f2b88317ea92e7
Kuldeep Saini <k72125642@gmail.com>
2025-10-24 22:44:57 +0530

    add- Barchart

--- app/components/RecordChart.tsx
+++ app/components/RecordChart.tsx
@@ -1,6 +1,8 @@
 
 import getRecords from '@/app/actions/getRecords';
-import BarChart from './BarChart'; // Ensure BarChart.tsx or BarChart.jsx exists in the same directory
+ //import BarChart from './BarChart'; // // Ensure BarChart.tsx or BarChart.jsx exists in the same directory
+
+import BarChart from './BarChart';
 
 const RecordChart = async () => {
   const { records, error } = await getRecords();
